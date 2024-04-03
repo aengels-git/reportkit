33c851f131d1ba8bedf3ce6bff6589de5647567a
u890640 <alexander.engels@dak.de>
2024-04-03 10:58:29 +0200

    added feature with page_navbar

--- R/report_nav_tabs.R
+++ R/report_nav_tabs.R
@@ -12,8 +12,54 @@ ggplot_to_html<-function(ggplot,width=7,height=3.5){
   return(blastula::add_ggplot(plot_object = ggplot,width=width,height=height))
 }
 
+#' report_nav_bar
+#'
+#' @param titles navbar titles
+#' @param bodies list of objects  
+#' @param height height of the container
+#'
+#' @return
+#' @export
+#'
+#' @examples
+report_nav_bar <- function(titles,bodies,nav_title="Navigation",
+                           full_screen = TRUE,
+                           height=4.6, width=NULL,
+                           gg_height=5, gg_width=8){
+  height <- height *  100
+  require(bslib)
+  nav_list <- pmap(list(
+    titles,
+    bodies
+  ),function(title,body){
+    if(class(body)[1] =="gg"){
+      body <- ggplot_to_html(body, height = gg_height, width = gg_width)
+    } else if (class(body)[1] %in% c("data.frame","tbl_df")){
+      body <- reactable(body)
+    } else if (class(body)[1] =="flextable"){
+      body <- body%>%
+        htmltools_value()
+    }
+    nav_panel(
+      title,
+      card(body, height=glue("{height}px"),full_screen = full_screen)
+    )
+    
+  })
+  result <- page_navbar(
+    title = nav_title,
+    !!!nav_list,
+    fluid = FALSE,
+    fillable=TRUE
+  )
+  if(is.null(width)){
+    return(result)
+  } else {
+    return(column(width,result))
+  }
+}
 
-#' prep_nav_tabs
+#' report_nav_tabs
 #'
 #' @param titles navbar titles
 #' @param bodies list of objects  
@@ -24,7 +70,9 @@ ggplot_to_html<-function(ggplot,width=7,height=3.5){
 #' @export
 #'
 #' @examples
-report_nav_tabs <- function(titles,bodies,footers=NULL,height=4.6, width=NULL){
+report_nav_tabs <- function(titles,bodies,footers=NULL,
+                            height=4.6, width=NULL,
+                            gg_height=5, gg_width=8){
   height <- height *  100
   require(bslib)
   if(is.null(footers)){
@@ -36,25 +84,28 @@ report_nav_tabs <- function(titles,bodies,footers=NULL,height=4.6, width=NULL){
     footers
   ),function(title,body,footer){
     if(class(body)[1] =="gg"){
-      body <- ggplot_to_html(body)
+      body <- ggplot_to_html(body, height = gg_height, width = gg_width)
     } else if (class(body)[1] %in% c("data.frame","tbl_df")){
       body <- reactable(body)
+    } else if (class(body)[1] =="flextable"){
+      body <- body%>%
+        htmltools_value()
     }
     if(is.null(footer)){
-      nav(
+      nav_panel(
         title,
-        card_body_fill(body)
+        card_body(body)
       )
     } else {
-      nav(
+      nav_panel(
         title,
-        card_body_fill(body),
+        card_body(body),
         card_footer(footer)
       )
     }
     
   })
-  result <- navs_tab_card(
+  result <- navset_card_tab(
     height = height, full_screen = FALSE,
     !!!nav_list
   )
--- test_html.R
+++ test_html.R
@@ -2,7 +2,9 @@ library(devtools)
 library(prepkit)
 library(viskit)
 library(reactable)
+library(rmarkdown)
 load_all()
+
 # Wie wird kontrolliert wo die Präsentation erstellt wird? Vermutlich aktueller Arbeitsornder
 report <- report_html()
 cat(report$header$create())
@@ -13,6 +15,38 @@ report$header$toc_float<-T
 # Abbildung ergänzen:
 tab_cut <- prep_freq_table(diamonds,cut)
 plot_cut <- vis_barplot(tab_cut,x=cut,y=n)
+
+# Ergänzen von Nav Tabs:
+nav_tabs <- report_nav_tabs(titles = c("tab_cut","plot_cut"),
+                            bodies = list(tab_cut%>%mutate_if(is.numeric,round,digits=2),
+                                          plot_cut),height = 6,width=10, gg_height = 10)
+
+report$add_object(nav_tabs,name = "nav_tabs")
+
+# For many tabs nav_bars are better suited:
+many_tabs <- report_nav_bar(titles = map_chr(seq(1000,50000,1000),~str_pad(.x, width=5,side = "left",pad = "0")),
+                            bodies = map(seq(1,50),function(id){tab_cut}),
+                            nav_title = "Top N",
+                            height = 6,width=10, gg_height = 10)
+
+report$add_object(many_tabs,name = "many_tabs")
+report$render()
+help(absolutePanel)
+nav_panel
+rstudioapi::viewer(nav_tabs)
+View(nav_tabs)
+card_body()
+page_navbar(
+  nav_panel("Page 2", card(ggplot_to_html(plot_cut), height = "100px",full_screen = FALSE)),
+  nav_panel("Page 2", card(ggplot_to_html(plot_cut), height = "100px",full_screen = FALSE))
+)
+
+report$render()
+report <- report_html()
+report$add_object()
+card(ggplot_to_html(plot_cut),height = "100px")
+
+help(page_navbar)
 report$add_object(plot_cut,header = "Balkendiagramm")
 report$add_break()
 
@@ -32,12 +66,7 @@ plotly_cut <- plotly::ggplotly(plot_cut)
 report$add_object(plotly_cut,header = "Interaktives Balkendiagramm")
 report$add_break()
 
-# Ergänzen von Nav Tabs:
-nav_tabs <- report_nav_tabs(titles = c("tab_cut","plot_cut"),
-                bodies = list(tab_cut%>%mutate_if(is.numeric,round,digits=2),
-                              plot_cut),height = 4,width=10)
 
-report$add_object(nav_tabs,name = "nav_Tabs")
 report$header$available_themes
 report$header$theme <- "lumen"
 report$render()
